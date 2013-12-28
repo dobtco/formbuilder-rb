5bb4cd8839cb35c402c5afa45f24cf423cf754cf
adamjacobbecker <ad@mbecker.cc>
2013-12-27 19:58:16 -0800

    form spec

--- spec/models/formbuilder/form_spec.rb
+++ spec/models/formbuilder/form_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe Formbuilder::Form do
 
-  pending
+  let(:form) { Formbuilder::Form.new }
+  subject { form }
+
+  it { should be_valid }
+  it { should respond_to(:formable) }
+  it { should respond_to(:response_fields) }
 
 end
