17ccc40698d38a10078a13f1c2efcf41275531ff
James Dullaghan <james@fldlvl.com>
2016-01-22 14:45:43 -0800

    Move label html

--- app/models/formbuilder/response_field_signature.rb
+++ app/models/formbuilder/response_field_signature.rb
@@ -10,17 +10,13 @@ module Formbuilder
         <label>#{self[:label]}</label>
       """
 
-      if self[:field_options][:description].present?
-        str += """<p>#{self[:field_options][:description]}</p>"""
-      end
-
       str += """
         <div id='signature-pad-#{self.id}' class='m-signature-pad'>
           <div class='m-signature-pad--body'>
             <canvas></canvas>
           </div>
           <div class='m-signature-pad--footer'>
-            <div class='description'>Sign above</div>
+            <div class='description'>#{self[:field_options][:description]}</div>
             <button type='button' class='button clear' data-action='clear'>Clear</button>
             <button type='button' class='button save' data-action='save'>Save</button>
           </div>
