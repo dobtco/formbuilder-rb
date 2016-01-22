8fdf2be5ac1de3e186e85751aa1ea5159f51ee51
James Dullaghan <james@fldlvl.com>
2016-01-22 11:08:21 -0800

    Remove yml files from version control
    Swap out keys...

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -4,8 +4,6 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-S3 = YAML.load_file(Rails.root.join('config', 's3.yml'))[Rails.env]
-
 # Don't be a dummy and follow the incorrect docs for hours like I did!
 # DOCS on github home are for master!
 # DOCS FOR v0.10.0 can be found here:
@@ -13,13 +11,13 @@ S3 = YAML.load_file(Rails.root.join('config', 's3.yml'))[Rails.env]
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id:     S3['access_key'],
-    aws_secret_access_key: S3['secret_key'],
-    region:                S3['region']
+    aws_access_key_id:     Rails.application.secrets.ec2_access_key,
+    aws_secret_access_key: Rails.application.secrets.ec2_access_secret,
+    region:                Rails.application.secrets.buccaneer_region
   }
 
   # Bucket Name
-  config.fog_directory = S3['bucket']
+  config.fog_directory = Rails.application.secrets.buccaneer_bucket
   config.fog_public = false
   config.fog_use_ssl_for_aws = true
   config.cache_dir = Rails.root.join('tmp', 'uploads')
